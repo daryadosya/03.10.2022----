52db898d289ef96f299021e7c145d0d69cb03a49
Darya <daryadosya@yandex.ru>
2022-10-03 16:20:18 +0300

    Написали Helllo world

---
+++ 000/hello.c
@@ -0,0 +1,16 @@
+/*!
+\authors     Дарья Лебедева <daryadosya@yandex.ru>
+\version     0.0.1a
+\date        03.10.2022
+\coopyright  MIT
+*/
+
+/// подключаем стандартную библиотеку
+#include <stdio.h>
+
+int hello (void){
+    /// Выводим в стандартный вывод надпись \
+        Hello world!!!
+    printf("Hello world!!!\n");
+    return 0;
+}
