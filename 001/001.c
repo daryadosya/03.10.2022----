927d9db5ce56dc50566aa5e74e626fa7b5bfda29
drayadosya <daryadosya@yandex.ru>
2022-10-05 11:54:05 +0300

    Пример с циклами

--- 001/001.c
+++ 001/001.c
@@ -7,6 +7,7 @@
 
 /// подключаем стандартную библиотеку
 #include <stdio.h>
+void s001 (int a);
 
 int start (void){
     /// Выводим в стандартный вывод надпись \
@@ -22,5 +23,17 @@ int start (void){
     printf("\nВведите переменную d: ");
     scanf ("%d", &d);
     printf("\nd =%d", d);
+    s001(d);
     return 0;
+}
+void s001 (int a){
+  if(a>0){
+    printf ("\nПеременная а: %d , больше 0.", a);
+  }
+  else if (a == 0){
+    printf ("\nПеременная а: %d , равна 0.", a);
+  }
+  else{
+    printf ("\nПеременная а: %d , меньше 0.", a);
+  }
 }
