d41f9e178e426b685de3c260958075777e7a7a48
drayadosya <daryadosya@yandex.ru>
2022-10-05 10:26:47 +0300

    Базовый шаблон-проект

---
+++ 001/001.h
@@ -0,0 +1,8 @@
+/*!
+\authors     Дарья Лебедева <daryadosya@yandex.ru>
+\version     0.0.1a
+\date        03.10.2022
+\coopyright  MIT
+*/
+
+int hello (void);
---
+++ 001/main.c
@@ -0,0 +1,14 @@
+/*!
+\authors     Дарья Лебедева <daryadosya@yandex.ru>
+\version     0.0.1a
+\date        03.10.2022
+\coopyright  MIT
+*/
+
+/// подключаем стандартную библиотеку
+#include <stdio.h>
+#include "hello.h"
+///Объявляем функцию main
+int main (void) {
+    return hello();
+}
